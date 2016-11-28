20d4768f5dba92c54a5bc519d1bff632ade16bcc
zhaozhuocheng <zhaozhuocheng@xiaomi.com>
2016-11-28 19:33:27 +0800

    Java&Cpp -Version

--- hmm.cpp
+++ hmm.cpp
@@ -1,34 +1,97 @@
-@@ -0,0 +1,158 @@
 #include <iostream>
 #include <vector>
 #include <map>
 #include <string>
 using namespace std;
+
 typedef vector< string > VecStr;
 typedef map< string, double > MapStrDou;
 typedef map< string, map< string, double > > MapStrMap;
 typedef vector< string > :: iterator VecStrI;
 typedef map< string, double > :: iterator MapStrDouI;
 typedef map< string, map< string, double > > :: iterator MapStrMapI;
-typedef struct DSD
-{
+
+typedef struct DSD {
 	double d1;
 	string s;
 	double d2;
 }DSD;
+
 typedef map< string, DSD > MapStrDSD;
+
 #define RAINY "Rainy"
 #define SUNNY "Sunny"
 #define WALK "walk"
 #define SHOP "shop"
 #define CLEAN "clean"
-DSD forward_viterbi(VecStr& obs,
-		VecStr& states,
-		MapStrDou& start_p,
-		MapStrMap& trans_p,
-		MapStrMap& emit_p);
-int main(void)
-{
+
+DSD forward_viterbi(VecStr& obs, VecStr& states, MapStrDou& start_p, MapStrMap& trans_p, MapStrMap& emit_p) {
+	MapStrDSD T;
+	VecStrI VS_i, VS_j, VS_k;
+	DSD DSD_tmp;
+	for (VS_i = states.begin(); VS_i != states.end(); VS_i++) {
+		DSD_tmp.d1 = start_p[*VS_i];
+		DSD_tmp.s = *VS_i;
+		DSD_tmp.d2 = start_p[*VS_i];
+		T[*VS_i] = DSD_tmp;
+	}
+	for (VS_i = obs.begin(); VS_i != obs.end(); VS_i++) {
+		MapStrDSD U;
+		for (VS_j = states.begin(); VS_j != states.end(); VS_j++) {
+			double total = 0.0;
+			string argmax = "";
+			double valmax = 0.0;
+			double prob = 1.0;
+			string v_path = "";
+			double v_prob = 1.0;
+			for (VS_k = states.begin(); VS_k != states.end(); VS_k++) {
+				DSD objs = T[*VS_k];
+				prob = objs.d1;
+				v_path = objs.s;
+				v_prob = objs.d2;
+				double p = (emit_p[*VS_k])[*VS_i] * (trans_p[*VS_k])[*VS_j];
+				prob *= p;
+				v_prob *= p;
+				total += prob;
+				if (v_prob > valmax) {
+					argmax = v_path + "," + *VS_j;
+					valmax = v_prob;
+				}
+			}
+			DSD_tmp.d1 = total;
+			DSD_tmp.s = argmax;
+			DSD_tmp.d2 = valmax;
+			U[*VS_j] = DSD_tmp;
+		}
+		T = U;
+	}
+
+	double total = 0.0;
+	string argmax = "";
+	double valmax = 0.0;
+	double prob = 1.0;
+	string v_path = "";
+	double v_prob = 1.0;
+	for (VS_i = states.begin(); VS_i != states.end(); VS_i++) {
+		DSD objs = T[*VS_i];
+		prob = objs.d1;
+		v_path = objs.s;
+		v_prob = objs.d2;
+		total += prob;
+		if (v_prob > valmax) {
+			argmax = v_path;
+			valmax = v_prob;
+		}
+	}
+	DSD_tmp.d1 = total;
+	DSD_tmp.s = argmax;
+	DSD_tmp.d2 = valmax;
+	
+	return DSD_tmp;
+}
+
+
+int main(void) {
 	VecStr states;
 	VecStr observations;
 	MapStrDou start_probability;
@@ -60,100 +123,10 @@ int main(void)
 	emission_probability[RAINY] = e1;
 	emission_probability[SUNNY] = e2;
 
-	DSD ret = forward_viterbi(observations,
-		states,
-		start_probability,
-		transition_probability,
-		emission_probability);
+	DSD ret = forward_viterbi(observations, states, start_probability, transition_probability, mission_probability);
 	cout << ret.d1 <<endl;
 	cout << ret.s << endl;
 	cout << ret.d2 << endl;
 
 	return 0;
-}
-DSD forward_viterbi(VecStr& obs,
-		VecStr& states,
-		MapStrDou& start_p,
-		MapStrMap& trans_p,
-		MapStrMap& emit_p)
-{
-	MapStrDSD T;
-	VecStrI VS_i, VS_j, VS_k;
-	DSD DSD_tmp;
-	for(VS_i = states.begin(); VS_i != states.end(); VS_i++)
-	{
-		DSD_tmp.d1 = start_p[*VS_i];
-		DSD_tmp.s = *VS_i;
-		DSD_tmp.d2 = start_p[*VS_i];
-		T[*VS_i] = DSD_tmp;
-	}
-	for(VS_i = obs.begin(); VS_i != obs.end(); VS_i++)
-	{
-		MapStrDSD U;
-		for(VS_j = states.begin(); VS_j != states.end(); VS_j++)
-		{
-			double total = 0.0;
-			string argmax = "";
-			double valmax = 0.0;
-			double prob = 1.0;
-			string v_path = "";
-			double v_prob = 1.0;
-			for(VS_k = states.begin(); VS_k != states.end(); VS_k++)
-			{
-				DSD objs = T[*VS_k];
-				prob = objs.d1;
-				v_path = objs.s;
-				v_prob = objs.d2;
-				double p = (emit_p[*VS_k])[*VS_i] * (trans_p[*VS_k])[*VS_j];
-				prob *= p;
-				v_prob *= p;
-				total += prob;
-				if(v_prob > valmax)
-				{
-					argmax = v_path + "," + *VS_j;
-					valmax = v_prob;
-				}
-				/** test */
-				cout << "obs = " << *VS_i << endl;
-				cout << "/tnext_state = " << *VS_j << endl;
-				cout << "/t/tstate = " << *VS_k << endl;
-				cout << "/t/t/tp = " << p << " = " << (emit_p[*VS_k])[*VS_i]
-				<< " * " << (trans_p[*VS_k])[*VS_j] << endl;
-				cout << "/t/t/t{prob, v_path, v_prob} = {"
-				<< prob << ", " << v_path << ", " << v_prob << "}" << endl;
-				cout << "/t/t/t{total, argmax, valmax} = {"
-				<< total << ", " << argmax << ", " << valmax << "}" << endl;
-				/** end of test */
-			}
-			DSD_tmp.d1 = total;
-			DSD_tmp.s = argmax;
-			DSD_tmp.d2 = valmax;
-			U[*VS_j] = DSD_tmp;
-		}
-		T = U;
-	}
-
-	double total = 0.0;
-	string argmax = "";
-	double valmax = 0.0;
-	double prob = 1.0;
-	string v_path = "";
-	double v_prob = 1.0;
-	for(VS_i = states.begin(); VS_i != states.end(); VS_i++)
-	{
-		DSD objs = T[*VS_i];
-		prob = objs.d1;
-		v_path = objs.s;
-		v_prob = objs.d2;
-		total += prob;
-		if(v_prob > valmax)
-		{
-			argmax = v_path;
-			valmax = v_prob;
-		}
-	}
-	DSD_tmp.d1 = total;
-	DSD_tmp.s = argmax;
-	DSD_tmp.d2 = valmax;
-	return DSD_tmp;
 }
